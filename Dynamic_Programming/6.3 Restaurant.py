d8d9608bdaee88289f019f41a6bbcf7b84b5d8e5
msun0106 <misuntech16@gmail.com>
2018-01-17 16:57:44 -0500

    Update 6.3 Restaurant.py

--- Dynamic_Programming/6.3 Restaurant.py
+++ Dynamic_Programming/6.3 Restaurant.py
@@ -1,3 +1,12 @@
+# 6.3. Yuckdonald's is considering opening a series of restaurants along Quaint Valley Highway (QVH).
+# The n possible locations are along a straight line, and the distances of these locations from the
+# start of QVH are, in miles and in increasing order,m1;m2; : : : ;mn. The constraints are as follows:
+#  At each location, Yuckdonald's may open at most one restaurant. The expected profit from
+# opening a restaurant at location i is pi, where pi > 0 and i = 1; 2; : : : ; n.
+#  Any two restaurants should be at least k miles apart, where k is a positive integer.
+# Give an effcient algorithm to compute the maximum expected total profit subject to the given
+# constraints.
+
 
 def open_res(locations, profit,k):
     num_res=len(locations)
@@ -20,11 +29,10 @@ def open_res(locations, profit,k):
     return P
 
 
-
-
-
 if __name__=='__main__':
     profits=[0,5,7,8,9]
     locati=[0,4,7,9,10]
     k=5
     print open_res(locati,profits,k)
+
+ 
