045afa498679697c2030cc374957e5365047e786
suns0106 <misuntech16@gmail.com>
2018-01-29 13:21:43 -0500

    Update Merge_Sort.py

--- Divide_and_Conquer/Merge_Sort.py
+++ Divide_and_Conquer/Merge_Sort.py
@@ -1,1 +1,30 @@
 
+def MergeSort(data):
+    if len(data)>1:
+        leftList=MergeSort(data[:len(data)/2])
+        rightList=MergeSort(data[len(data)/2:])
+        return merge(leftList, rightList)
+    else:
+        return data
+
+def merge(leftList, rightList):
+    result,i,j=[],0,0
+
+    for k in range(len(leftList)+len(rightList)):
+          if i <len(leftList) and j <len(rightList):
+              if leftList[i]<=rightList[j]:
+                  result.append(leftList[i])
+                  i+=1
+              else:
+                  result.append(rightList[j])
+                  j+=1
+          else:
+              result.append(leftList[i] if i <len(leftList) else rightList[j])
+              i+=1
+              j+=1
+    #print result
+    return result
+
+if __name__=='__main__':
+     result=MergeSort([2,4,7,5,9,8,1,3,6])
+     print result
