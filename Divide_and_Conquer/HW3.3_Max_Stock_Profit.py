a86701d72ee29e5f5ce277712a8470393a5e98a4
ms2ag16 <misuntech16@gmail.com>
2018-01-30 13:28:30 -0500

    Update HW3.3_Max_Stock_Profit.py

--- Divide_and_Conquer/HW3.3_Max_Stock_Profit.py
+++ Divide_and_Conquer/HW3.3_Max_Stock_Profit.py
@@ -1,3 +1,4 @@
+"""O(nlogn)"""
 def Max_profit(data,left,right):
     if left >= right:
         return 0
@@ -17,7 +18,28 @@ def Max_profit(data,left,right):
 
     return LeftMaxProfit,RightMaxProfit,highest-lowest
 
+"""O(n)"""
+def Max_profit2(data,left,right):
+    if left >= right:
+        return (0,data[left],data[right])
+
+    mid=int((left+right)/2)
+    highest=lowest=data[mid]
+
+    """ recursively call Max_profit on Left/Right side"""
+    LeftMaxProfit, highestLeft, lowestLeft=Max_profit2(data,left,mid)
+
+    RightMaxProfit, highestRight, lowestRight=Max_profit2(data,mid+1,right)
+
+    """ calculate on the third senario: lowest in left, highest in right"""
+    max_Profit=max(LeftMaxProfit, RightMaxProfit,highestRight-lowestLeft)
+
+
+    return max_Profit, max(highest,highestRight,highestLeft),min(lowest,lowestLeft,lowestRight)
+
+
 if __name__=="__main__":
     price=[2,9,5,-3,8,9,-2]
     n=len(price)-1
     print Max_profit(price,0,n)
+    print Max_profit2(price,0,n)
