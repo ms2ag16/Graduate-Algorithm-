30aba6ccaabc511e2d8e92b1cfd5efa7cc489bb9
ms2ag16 <misuntech16@gmail.com>
2018-02-01 15:02:03 -0500

    Update LC-Paint_House.py

--- Dynamic_Programming/LC-Paint_House.py
+++ Dynamic_Programming/LC-Paint_House.py
@@ -5,3 +5,18 @@
 """ The cost of painting each house with a certain color is represented by a n x 3 cost matrix. """
 """ For example, costs[0][0] is the cost of painting house 0 with color red;"""
 """ costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses."""
+def Paint_house(costs):
+    """
+    :param costs: n*3 matrix, 3 -- 3 colors, n = number of house
+    :return: min cost for paint n house
+
+    """
+    if(costs.empty() or costs[0].empty):
+        return 0
+
+    pcosts=costs
+    for i in range(1,pcosts.size):
+        for j in range(3):
+            pcosts[i][j] += min(pcosts[i-1][(j+1)%3],pcosts[i-1][(j+2)%3])
+    
+    return min(pcosts[len(costs)-1])
