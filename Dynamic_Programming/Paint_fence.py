5a6c3e770060cad15ab1a1666183891f123332c0
ms2ag16 <misuntech16@gmail.com>
2018-02-01 13:59:22 -0500

    Create Paint_fence.py

---
+++ Dynamic_Programming/Paint_fence.py
@@ -0,0 +1,3 @@
+""" There is a fence with n posts, each post can be painted with one of the k colors."""
+""" You have to paint all the posts such that no more than two adjacent fence posts have the same color."""
+""" Return the total number of ways you can paint the fence."""
