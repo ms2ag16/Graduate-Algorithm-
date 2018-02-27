4a22e774a55cc974e0bb1dd3487c287e16162848
ms2ag16 <misuntech16@gmail.com>
2018-02-27 14:54:23 -0500

    Update Clone Graph.py

--- Graph Algorithms/Clone Graph.py
+++ Graph Algorithms/Clone Graph.py
@@ -1,1 +1,23 @@
+# dfs implement
 
+class UndirectedGraphNode:
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
+
+def cloneGraph(node):
+        if not node:
+            return node
+        root=UndirectedGraphNode(node.label)
+        stack=[node]
+        visit={}
+        visit[node.label]=root
+        while stack:
+            top=stack.pop()
+            for n in top.neighbors:
+                if n.label not in visit:
+                    stack.append(n)
+                    visit[n.label]=UndirectedGraphNode(n.label)
+                visit[top.label].neighbors.append(visit[n.label])
+
+        return root
