820f893f42f4e9b4d36ae577344738a2c6607aa4
ms2ag16 <misuntech16@gmail.com>
2018-05-22 10:21:56 -0400

    Update 001. Two Sum.py

--- Leetcode/001. Two Sum.py
+++ Leetcode/001. Two Sum.py
@@ -16,3 +16,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # dict store the index of each element in nums
+        dict={}
+        for i in range(len(nums)):
+            x=nums[i]
+            if target-x in dict:
+                return (dict[target-x], i)
+            dict[x]=i
+
+if __name__=="__main__":
+    print(twoSum([2, 7, 11, 15], 26))
