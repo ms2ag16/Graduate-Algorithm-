0646db3981be0d5c8f7047176305dcebde31a07a
ms2ag16 <misuntech16@gmail.com>
2018-05-24 14:53:28 -0400

    Update 024. Swap Nodes in Pairs.py

--- Leetcode/Linked List/024. Swap Nodes in Pairs.py
+++ Leetcode/Linked List/024. Swap Nodes in Pairs.py
@@ -8,7 +8,11 @@ Note:
 
 Your algorithm should use only constant extra space.
 You may not modify the values in the list's nodes, only nodes itself may be changed.
-
+比较常见的链表操作。下面看一下典型情况，如要交换链表中A->B->C->D中的B和C需要做如下操作：
+将A指向C
+将B指向D
+将C指向B
+在头节点之前加一个假节点就可以使所有的交换都符合上面的情况。
 """
 
 # Definition for singly-linked list.
