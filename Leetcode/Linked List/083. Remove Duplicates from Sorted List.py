424723442c002c058b2655bde90dc79bc18643ee
ms2ag16 <misuntech16@gmail.com>
2018-06-04 15:27:32 -0400

    Create 083. Remove Duplicates from Sorted List.py

---
+++ Leetcode/Linked List/083. Remove Duplicates from Sorted List.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
