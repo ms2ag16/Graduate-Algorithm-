c85404361acf4162215e2ce7b1bcf43a4793baff
ms2ag16 <misuntech16@gmail.com>
2018-06-05 09:53:29 -0400

    Create 082. Remove Duplicates from Sorted List II.py

---
+++ Leetcode/Linked List/082. Remove Duplicates from Sorted List II.py
@@ -0,0 +1,8 @@
+""" 
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+Example 1:
+
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+"""
