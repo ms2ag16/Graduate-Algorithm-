cafd36a039f57c1f88233fa893b248c9d0f76779
ms2ag16 <misuntech16@gmail.com>
2018-06-06 13:17:38 -0400

    Update 061. Rotate List.py

--- Leetcode/Linked List/061. Rotate List.py
+++ Leetcode/Linked List/061. Rotate List.py
@@ -25,3 +25,34 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
+        dummy=ListNode(0)
+        dummy.next=head
+        p=dummy
+        count=0
+        if not head:
+            return head
+        while p.next:
+            p=p.next
+            count+=1
+        shift=count-k%count
+        p.next=head
+        for i in range(0,shift):
+            p=p.next
+        head=p.next
+        p.next=None
+        return head
+
+ if __name__ == "__main__":
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l3 = ListNode(3)
+    l4 = ListNode(4)
+    l5 = ListNode(5)
+    l1.next = l2
+    l2.next = l3
+    l3.next = l4
+    l4.next = l5
+    result = Solution().rotateRight(l1, 2)
+    result.myprint()          
+            
+       
