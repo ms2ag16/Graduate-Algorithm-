940d6680882104467403f43084b097bc4c9eb104
ms2ag16 <misuntech16@gmail.com>
2018-06-20 12:50:57 -0400

    Update 160. Intersection of Two Linked Lists.py

--- Leetcode/Linked List/160. Intersection of Two Linked Lists.py
+++ Leetcode/Linked List/160. Intersection of Two Linked Lists.py
@@ -9,3 +9,27 @@ A:          a1 → a2
                    ↗            
 B:     b1 → b2 → b3
 """
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA is None or headB is None:
+            return None
+
+        pointA=headA
+        pointB=headB
+
+        while pointA is not pointB:
+            pointA=headB if pointA is None else pointA.next
+            pointB=headA if pointB is None else pointB.next
+
+        return pointA
