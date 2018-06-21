2e1cbcd0744c60df18843a9652c1706e3f0a18a4
ms2ag16 <misuntech16@gmail.com>
2018-06-21 15:19:49 -0400

    Update 001. Two Sum.py

--- Array/001. Two Sum.py
+++ Array/001. Two Sum.py
@@ -10,3 +10,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict={}
+        for i in range(len(nums)):
+            x=nums[i]
+            if target-x in dict:
+                return (dict[target-x], i)
+            dict[x]=i
+            
