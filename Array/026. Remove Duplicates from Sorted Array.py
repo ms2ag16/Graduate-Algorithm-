889e77b6b06a5a7f6935a29207d4d127221716bd
ms2ag16 <misuntech16@gmail.com>
2018-06-26 13:30:19 -0400

    Update 026. Remove Duplicates from Sorted Array.py

--- Array/026. Remove Duplicates from Sorted Array.py
+++ Array/026. Remove Duplicates from Sorted Array.py
@@ -11,3 +11,26 @@ Your function should return length = 2, with the first two elements of nums bein
 
 It doesn't matter what you leave beyond the returned length.
 """
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:
+          return 0
+        start=0
+        for i in range(1,len(nums)):
+            if nums[start]!=nums[i]:
+                start+=1
+                nums[start]=nums[i]
+                print ('start={0}, nums[start]={0}'.format(start, nums[start]))
+        return start+1
+
+
+
+if __name__ == "__main__":
+    print Solution().removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+      
+      
