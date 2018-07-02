0ed08e43f6c70f51a25a1f7e38d4655bd2379ee0
ms2ag16 <misuntech16@gmail.com>
2018-07-02 11:23:32 -0400

    Update 015. 3Sum.py

--- Array/015. 3Sum.py
+++ Array/015. 3Sum.py
@@ -14,3 +14,29 @@ A solution set is:
   [-1, -1, 2]
 ]
 """
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            """ remove duplicate"""
+            if i >= 1 and v == nums[i-1]:
+                print ("{0} is {1}".format(i,v))
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+        return map(list, res)
+
+
+if __name__ == "__main__":
+    print Solution().threeSum([-1, 0, 1, 2, -1, -4] )
