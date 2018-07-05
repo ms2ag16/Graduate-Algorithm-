b8118d3667bc3e0880465b668d96b5dc32100c03
ms2ag16 <misuntech16@gmail.com>
2018-07-05 16:33:08 -0400

    Update 039. Combination Sum.py

--- Array/039. Combination Sum.py
+++ Array/039. Combination Sum.py
@@ -15,3 +15,33 @@ A solution set is:
   [2,2,3]
 ]
 """
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if not candidates:
+            return []
+        candidates.sort()
+        result=[]
+        self.dfs(candidates, target, [], result)
+        return result
+    
+    def dfs(self, candidates, target, cur_num, result):
+        s=sum(cur_num) if current else 0
+        if s >target:
+            return
+        elif s==target:
+            result.append(cur_num)
+        else:
+            for i, v in enumerate (candidates):
+                self.dfs(candidates[i:], target, cur_num+[v], result)
+     
+if __name__ == "__main__":
+    print Solution().combinationSum([2, 3, 6, 7], 7)
+ 
+ 
+        
