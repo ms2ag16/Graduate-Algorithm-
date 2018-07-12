89002238b4d7e398f226f7f32832af8b779cb276
ms2ag16 <misuntech16@gmail.com>
2018-07-12 11:09:02 -0400

    Update 081. Search in Rotated Sorted Array II.py

--- Array/081. Search in Rotated Sorted Array II.py
+++ Array/081. Search in Rotated Sorted Array II.py
@@ -18,4 +18,29 @@ class Solution(object):
         :rtype: bool
         """
         
+        left=0
+        right=len(nums)-1
+        
+        while left<=right:
+            mid=(left+right)/2
+            
+            if nums[mid]==target:
+                return True
+            
+            if nums[left]==nums[right]==nums[mid]:
+                left+=1
+                right-=1
+            elif nums[mid]>nums[left]: # left part is organized
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if nums[mid]<target<=nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return False
+                
+        
 
