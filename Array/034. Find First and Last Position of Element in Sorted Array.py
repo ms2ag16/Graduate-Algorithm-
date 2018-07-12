b705c809a1da90e12c68fc4c91deb6a3e5c0c087
ms2ag16 <misuntech16@gmail.com>
2018-07-12 14:32:43 -0400

    Update 034. Find First and Last Position of Element in Sorted Array.py

--- Array/034. Find First and Last Position of Element in Sorted Array.py
+++ Array/034. Find First and Last Position of Element in Sorted Array.py
@@ -14,3 +14,34 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+      
+        result = []
+        length = len(nums)
+
+        start = 0
+        end = length
+
+        while start < end:
+            mid = (start + end) / 2
+            if nums[mid] == target and (mid == 0 or nums[mid - 1] != target):
+                result.append(mid)
+                break
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid
+        if not result:
+            return [-1,-1]
+
+        end = length
+        while start < end:
+            mid = (start + end) / 2
+            if nums[mid] == target and (mid==length-1 or nums[mid+1] != target):
+                result.append(mid)
+                break
+            if nums[mid] <= target:
+                start = mid + 1
+            else:
+                end = mid
+
+        return result
