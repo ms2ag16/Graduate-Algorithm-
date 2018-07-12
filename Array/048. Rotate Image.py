311e2b52dfce761dfd37492cb7be30a522ba0c99
ms2ag16 <misuntech16@gmail.com>
2018-07-12 16:24:06 -0400

    Update 048. Rotate Image.py

--- Array/048. Rotate Image.py
+++ Array/048. Rotate Image.py
@@ -22,3 +22,19 @@ rotate the input matrix in-place such that it becomes:
 ]
 """
 
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n=len(matrix)
+        for row in range(n):
+            for column in range(n-row):
+                matrix[row][column], matrix[n-1-column][n-1-row]=matrix[n-1-column][n-1-row],matrix[row][column]
+
+        for row in range(n//2):
+            for column in range(n):
+                matrix[row][column], matrix[n-1-row][column]=matrix[n-1-row][column], matrix[row][column]
+      
+      return matrix
