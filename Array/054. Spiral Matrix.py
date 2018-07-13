e328272a71f7c011a9570878ecc3c6f1d08883d6
ms2ag16 <misuntech16@gmail.com>
2018-07-13 09:47:12 -0400

    Create 054. Spiral Matrix.py

---
+++ Array/054. Spiral Matrix.py
@@ -0,0 +1,12 @@
+"""
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+"""
