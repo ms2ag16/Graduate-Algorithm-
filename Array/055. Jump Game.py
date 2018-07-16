e5e9a95dbbd40034c39698ec20c5dec49a48237c
ms2ag16 <misuntech16@gmail.com>
2018-07-16 15:42:41 -0400

    Create 055. Jump Game.py

---
+++ Array/055. Jump Game.py
@@ -0,0 +1,10 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+"""
