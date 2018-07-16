0e46e6f13cd3bdac84d773a11798d2edfb9e1cfa
ms2ag16 <misuntech16@gmail.com>
2018-07-16 14:37:05 -0400

    Update 059. Spiral Matrix II.py

--- Array/059. Spiral Matrix II.py
+++ Array/059. Spiral Matrix II.py
@@ -10,3 +10,41 @@ Output:
  [ 7, 6, 5 ]
 ]
 """
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        left=top=0
+        right=n-1
+        bottom = n-1
+        num=1
+        result=[[0 for _ in range(n)] for _ in range (n)]
+
+        while left<right and top<bottom:
+            for i in range(left, right):
+                result[top][i]=num
+                num+=1
+            for i in range(top,bottom):
+                result[i][right]=num
+                num+=1
+            for i in range(right,left,-1):
+                result[bottom][i]=num
+                num+=1
+            for i in range(bottom, top,-1):
+                result[i][left]=num
+                num+=1
+            left+=1
+            right-=1
+            top+=1
+            bottom-=1
+        if n%2==1:
+            result[top][left]=num
+            print num
+        return result
+
+
+
+if __name__=='__main__':
+    print Solution().generateMatrix(5)
