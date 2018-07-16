8853ed56693574ede4ae1dd642ae8bce67b70da4
ms2ag16 <misuntech16@gmail.com>
2018-07-16 16:04:57 -0400

    Update 055. Jump Game.py

--- Array/055. Jump Game.py
+++ Array/055. Jump Game.py
@@ -8,3 +8,25 @@ Input: [2,3,1,1,4]
 Output: true
 Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 """
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        length=len(nums)
+        index=0
+        longest=nums[0]
+        while index <=longest:
+            if longest>=length-1:
+                return True
+            longest=max(longest,index+nums[index])
+            index+=1
+        return  False
+
+if __name__ == "__main__":
+    print Solution().canJump([2, 3, 1, 1, 4])
