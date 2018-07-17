196742af88742a9ea7e1d35a879baff6ca7a0817
ms2ag16 <misuntech16@gmail.com>
2018-07-17 11:55:09 -0400

    Create 073. Set Matrix Zeroes.py

---
+++ Array/073. Set Matrix Zeroes.py
@@ -0,0 +1,17 @@
+"""
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+Example 1:
+Input: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+"""
