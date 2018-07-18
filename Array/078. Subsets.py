2ed8cede7353a16b29a7cc24024823ac4ba5a99a
ms2ag16 <misuntech16@gmail.com>
2018-07-18 13:34:38 -0400

    Update 078. Subsets.py

--- Array/078. Subsets.py
+++ Array/078. Subsets.py
@@ -24,4 +24,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        result=[[]]
+        for num in sorted(nums):
+          result+=[item+[num] for item in result]
+        return result
         
