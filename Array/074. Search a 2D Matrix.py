6366e8b3c30bbcb03b448d521455bb70bd955683
ms2ag16 <misuntech16@gmail.com>
2018-07-18 12:11:50 -0400

    Update 074. Search a 2D Matrix.py

--- Array/074. Search a 2D Matrix.py
+++ Array/074. Search a 2D Matrix.py
@@ -21,3 +21,31 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        if not matrix:
+            return False
+        
+        nrow = len(matrix)
+        ncol = len(matrix[0])
+
+        low, high = 0, nrow * ncol - 1
+        while low <= high:
+            mid = (low + high) / 2
+            if matrix[mid// ncol][mid % ncol] == target:
+                return True
+            elif matrix[mid//ncol][mid % ncol] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
+
+
+
+if __name__ == "__main__":
+    print Solution().searchMatrix( [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+], 7)
+
+
+        
