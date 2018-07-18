249430a5ca4f73436f49d2fa3f68e0d6671ebd41
ms2ag16 <misuntech16@gmail.com>
2018-07-18 11:47:13 -0400

    Update 073. Set Matrix Zeroes.py

--- Array/073. Set Matrix Zeroes.py
+++ Array/073. Set Matrix Zeroes.py
@@ -22,3 +22,35 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
+        first_row=first_col=False
+        nrow=len(matrix)
+        ncol=len(matrix[0])
+        
+        for i in range(nrow):
+            if matrix[i][0]==0:
+                first_col=True
+        for i in range(ncol):
+            if matrix[0][i]==0:
+                first_row=True
+        
+        for row in range(1,nrow):
+            for col in range(1,ncol):
+                if matrix[row][col]==0:
+                    matrix[row][0]=matrix[0][col]=0 """ mark the correspoding first_row/col to 0"""
+        """ loop the matrix, to change the numbers"""
+        for row in range(1,nrow):
+            for col in range(1,ncol):  
+                if matrix[0][col]==0 or matrix[row][0]==0:
+                    matrix[row][col]=0
+        
+        """ deal with the first_row and first_col"""
+        if first_row:
+            for col in range(ncol):
+                matrix[0][col]=0
+        if first_col:
+            for row in range(nrow):
+                matrix[row][0]=0
+
+                    
+                
+                
