cdf812bf27e9ad64b42ac604f6886de8bb30cdc7
ms2ag16 <misuntech16@gmail.com>
2018-07-18 13:03:42 -0400

    Update 075. Sort Colors.py

--- Array/075. Sort Colors.py
+++ Array/075. Sort Colors.py
@@ -21,4 +21,24 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
+        3 pointers left, mid, right
+        left and right to mark the subfivision of red and blue
+        while mid is the one go over the loop of the list
         """
+        left=mid=0
+        right=len(nums)-1
+        while mid<=right:
+            if nums[mid]==0:
+                nums[left],nums[mid]=nums[mid],nums[left]
+                left+=1
+                nums+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[right],nums[mid]=nums[mid],nums[right]
+                right-=1
+                
+if __name__ == "__main__":
+    l = [1, 2, 1, 2, 0, 2, 1, 0, 2, 0, 0, 2]
+    Solution().sortColors(l)
+    assert l == [0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2]
