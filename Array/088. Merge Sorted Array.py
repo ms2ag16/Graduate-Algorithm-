fa73208e55f5546ae0b38f5e8f4a8925e5a0e5d7
ms2ag16 <misuntech16@gmail.com>
2018-07-19 16:25:09 -0400

    Update 088. Merge Sorted Array.py

--- Array/088. Merge Sorted Array.py
+++ Array/088. Merge Sorted Array.py
@@ -23,3 +23,16 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
+        index=m+n-1
+        m-=1
+        n-=1
+        while m>=0 and n>=0:
+            if nums1[m]>nums2[n]:
+                nums1[index]=nums1[m]
+                m-=1
+            else:
+                nums1[index]=nums2[n]
+            index-=1
+        while m<0:
+            nums1[:n+1]=nums2[:n+1]
+            
