85d5411ebe4978c24b93dbcc7ec1584f9692f548
ms2ag16 <misuntech16@gmail.com>
2018-07-19 14:08:12 -0400

    Update 079. Word Search.py

--- Array/079. Word Search.py
+++ Array/079. Word Search.py
@@ -1,7 +1,8 @@
 """
 Given a 2D board and a word, find if the word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or
+vertically neighboring. The same letter cell may not be used more than once.
 
 Example:
 
@@ -24,3 +25,5 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
+        
+        
