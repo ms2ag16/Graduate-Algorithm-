2eb23ff3ad392fba47a6dff0c6d88d8356e9c800
ms2ag16 <misuntech16@gmail.com>
2018-07-26 09:34:16 -0400

    Update 118. Pascal's Triangle.py

--- Array/118. Pascal's Triangle.py
+++ Array/118. Pascal's Triangle.py
@@ -20,3 +20,21 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        pascal_rows=[]
+        for i in range(numRows):
+            if i==0:
+                pascal_rows.append([1])
+            elif i==1:
+                pascal_rows.append([1,1])
+            else:
+                cur_row=[1]
+                for j in range(len(pascal_rows[i-1])-1):
+                    cur_row_num=pascal_rows[i-1][j]+pascal_rows[i-1][j+1]
+                    cur_row.append(cur_row_num)
+                cur_row.append(1)
+                pascal_rows.append(cur_row)
+        return pascal_rows
+    
+ if __name__ == "__main__":
+    print Solution().generate(4)              
+          
