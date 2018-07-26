f31502de79927daaa58793388898c9895eab71c2
ms2ag16 <misuntech16@gmail.com>
2018-07-26 13:36:35 -0400

    Update 120. Triangle.py

--- Array/120. Triangle.py
+++ Array/120. Triangle.py
@@ -25,7 +25,19 @@ class Solution(object):
             for j in range(i+1):
                 dp[j]=triangle[i][j]+min(dp[j],dp[j+1])
         return dp[0]
- 
+
+     def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle:
+            return 0
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+        return triangle[0][0]
  
 if __name__ == "__main__":
     print Solution().minimumTotal([
