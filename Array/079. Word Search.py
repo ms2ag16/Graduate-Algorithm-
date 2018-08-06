c7f9da3686c7b26d11666d5b9e26b70f3a9b66d9
ms2ag16 <misuntech16@gmail.com>
2018-08-06 14:14:10 -0400

    Update 079. Word Search.py

--- Array/079. Word Search.py
+++ Array/079. Word Search.py
@@ -25,5 +25,41 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        
+        if self._hasEnoughCharacters(board,word):
+            nrow=len(board)
+            ncol=len(board[0])
+            for i in range(nrow):
+                for j in range(ncol):
+                    if self._exist(board, i, j, nrow, ncol, word):
+                        return  True
+        return False
+
+
+
+
+    def _exist(self,board,i,j,nrow,ncol,word):
+        if len(word)==0:
+            return True
+        if i<0 or i>=nrow or j<0 or j>=ncol or board[i][j]!=word[0]:
+            return False
+        temp=board[i][j]
+        board[i][j]='.'
+        next_target=word[1:]
+        next_result=self._exist(board,i-1,j,nrow,ncol, next_target) or self._exist(board,i+1,j,nrow,ncol, next_target) or \
+                 self._exist(board, i , j-1, nrow, ncol, next_target) or  self._exist(board,i,j+1,nrow,ncol, next_target)
+        board[i][j]=temp
+        return next_result
+
+
+
+
+
+    def _hasEnoughCharacters(self, board, word):
+        from collections import defaultdict
+
+        character_counts = defaultdict(int)
+        for ch in word:
+            character_counts[ch] += 1
+
+        return all(sum(map(lambda line: line.count(ch), board)) >= count for ch, count in character_counts.items())
         
