650fff229fc29406528c9722778e53b081afd71c
ms2ag16 <misuntech16@gmail.com>
2018-08-06 12:21:19 -0400

    Update 169. Majority Element.py

--- Array/169. Majority Element.py
+++ Array/169. Majority Element.py
@@ -22,3 +22,31 @@ class Solution(object):
         nums.sort()
         return nums[len(nums)//2]
         
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==1:
+            return nums[0]
+        return self.majority(nums,0,len(nums)-1)
+    
+    def majority(self, nums, left, right):
+        if left==right:
+            return nums[left]
+        mid=(left+right)/2
+        m_left=self.majority(nums, left, mid)
+        m_right=self.majority(nums, mid+1,right)
+        
+        if m_left==m_right:
+            return m_left
+        
+        left_count=right_count=0
+        for i in range(left, right+1):
+            if nums[i]==m_left:
+                left_count+=1
+            elif nums[i]==m_right:
+                right_count+=1
+        return m_left if left_count>right_count else m_right
+    
