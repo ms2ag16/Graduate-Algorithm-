c755bc99a76bb22393a39df4b344c47e777d67bb
ms2ag16 <misuntech16@gmail.com>
2018-08-06 12:22:43 -0400

    Create 189. Rotate Array.py

---
+++ Array/189. Rotate Array.py
@@ -0,0 +1,19 @@
+"""
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+"""
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
