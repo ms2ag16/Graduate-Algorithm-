cf0c4cf907e91d3305985cf510733718572c6f97
ms2ag16 <misuntech16@gmail.com>
2018-08-06 12:46:20 -0400

    Update 189. Rotate Array.py

--- Array/189. Rotate Array.py
+++ Array/189. Rotate Array.py
@@ -17,3 +17,14 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        def reverse(nums,left,right):
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                start+=1
+                end-=1
+        n=len(nums)
+        k%=n
+        reverse(nums,0,n-k-1)
+        reverse(nums,n-k,n-1)
+        reverse(nums,0,n-1)
+        
