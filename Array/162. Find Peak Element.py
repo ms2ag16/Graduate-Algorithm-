c82d58596b121978f7a0816c6c9a2a955cbad35c
ms2ag16 <misuntech16@gmail.com>
2018-08-07 15:32:29 -0400

    Create 162. Find Peak Element.py

---
+++ Array/162. Find Peak Element.py
@@ -0,0 +1,15 @@
+"""
+A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+"""
