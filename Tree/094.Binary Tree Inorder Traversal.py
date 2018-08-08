67a53ff58f006b70121881b0ac723d81ea230d59
ms2ag16 <misuntech16@gmail.com>
2018-08-08 11:30:17 -0400

    Update 094.Binary Tree Inorder Traversal.py

--- Tree/094.Binary Tree Inorder Traversal.py
+++ Tree/094.Binary Tree Inorder Traversal.py
@@ -25,3 +25,28 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        result=[]
+        stack=[]
+        p=root
+        while p or stack:
+            # save the node which have left child
+            while p:
+                stack.append(p)
+                p=p.left
+            if stack:
+                p=stack.pop()
+                # visit the (left node, root) 
+                result.append(p.val)
+                #visit the right subtree
+                p=p.right
+        return result
+
+
+if __name__ == "__main__":
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n3 = TreeNode(3)
+    n1.right = n2
+    n2.left = n3
+    print  Solution().inorderTraversal(n1)                
+                
