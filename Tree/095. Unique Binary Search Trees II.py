df957377259fea4ddded809f6fb6d5def3864250
ms2ag16 <misuntech16@gmail.com>
2018-08-08 14:15:31 -0400

    Update 095. Unique Binary Search Trees II.py

--- Tree/095. Unique Binary Search Trees II.py
+++ Tree/095. Unique Binary Search Trees II.py
@@ -35,5 +35,24 @@ class Solution(object):
         :type n: int
         :rtype: List[TreeNode]
         """
+        if n == 0:
+            return []
+        self.cache = {}
+        return self._generateTrees(1, n)
+
+    def _generateTrees(self, start, end):
+        if (start, end) not in self.cache:
+            roots = []
+            for root in range(start, end + 1):
+                for left in self._generateTrees(start, root - 1):
+                    for right in self._generateTrees(root + 1, end):
+                        node = TreeNode(root)
+                        node.left = left
+                        node.right = right
+                        roots.append(node)
+            self.cache[(start, end)] = roots
+        return self.cache[(start, end)] or [None]
+
+        
         
         
