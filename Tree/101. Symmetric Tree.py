c0296b3edcbe5d837bd009be83da3b28e8be337f
ms2ag16 <misuntech16@gmail.com>
2018-08-13 10:17:12 -0400

    Update 101. Symmetric Tree.py

--- Tree/101. Symmetric Tree.py
+++ Tree/101. Symmetric Tree.py
@@ -45,5 +45,36 @@ class Solution(object):
             return False
         return self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
     
-    #solve it iterative
+#solve it iterative
+class Solution2(object):
+    # Solve it iteratively
+    def isSymmetric_iterate(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        stack1, stack2=[],[]
+        stack1.append(root.left)
+        stack2.append(root.right)
+        while stack1 and stack2:
+            size1=len(stack1)
+            size2=len(stack2)
+            if size1!=size2:
+                return False
+            for _ in range(size1):
+                curr1, curr2=stack1.pop(),stack2.pop()
+                if not curr1 and not curr2:
+                    continue
+                if not curr1 or not curr2:
+                    return False
+                if curr1.val != curr2.val:
+                    return False
+                stack1.append(curr1.left)
+                stack1.append(curr1.right)
+                stack2.append(curr2.right)
+                stack2.append(curr2.left)
+        return not stack1 and not stack2
+    
     
