3ae01a6915dfa50b4d1e9a3dfc2ba7eb40fcddd9
ms2ag16 <misuntech16@gmail.com>
2018-08-15 12:19:06 -0400

    Update 111. Minimum Depth of Binary Tree.py

--- Tree/111. Minimum Depth of Binary Tree.py
+++ Tree/111. Minimum Depth of Binary Tree.py
@@ -30,3 +30,31 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        if root is None:
+            return 0
+        depth, curr_level=0,[root]
+        while curr_level:
+            depth+=1
+            next_level=[]
+            for node in curr_level:
+                left, right=node.left, node.right
+                if left is None and right is None:
+                    return depth
+                if left:
+                    next_level.append(left)
+                if right:
+                    next_level.append(right)
+                curr_level=next_level
+        return depth
+ 
+if __name__ == "__main__":
+    n1 = TreeNode(0)
+    n2 = TreeNode(-3)
+    n3 = TreeNode(9)
+    n4 = TreeNode(-10)
+    n5 = TreeNode(5)
+    n1.right = n3
+    n1.left = n2
+    n2.left=n4
+    n3.left=n5
+    print  Solution().minDepth(n1)
