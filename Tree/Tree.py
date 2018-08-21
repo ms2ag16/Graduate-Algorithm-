a3f4d84b5e83063f57cccdebf63df7ecbc2c53db
ms2ag16 <misuntech16@gmail.com>
2018-08-21 13:12:26 -0400

    Update Tree.py

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -1,1 +1,17 @@
+class TreeNode(object):
+  def __init__(self, x=-1, left=None, right=None):
+    self.val=x
+    self.left=left
+    self.right=right
 
+class Tree(object):
+  def __init__(self):
+    self.root=TreeNode()
+    self.myQueue=[]
+  
+  def add(self, val):
+    node=TreeNode(val)
+    if self.root.val==-1:
+      self.root=node
+      self.myQueue.append(self.root)
+      
