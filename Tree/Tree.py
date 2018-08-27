106f6b61fe61c5405a0a6667c1bfb99bd7c756dd
ms2ag16 <misuntech16@gmail.com>
2018-08-27 15:33:22 -0400

    Update Tree.py

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -2,7 +2,7 @@ class TreeNode(object):
   def __init__(self, x=-1, left=None, right=None):
     self.val=x
     self.left=left
-    self.right=right
+    self.right=rig
 
 class Tree(object):
   def __init__(self):
@@ -10,9 +10,23 @@ class Tree(object):
     self.myQueue=[]
   
   def add(self, val):
-    node=TreeNode(val)
-    if self.root.val==-1:
-      self.root=node
-      self.myQueue.append(self.root)
+      node=TreeNode(val)
+      if self.root.val==-1:
+          self.root=node
+          self.myQueue.append(self.root)
+      else:
+          treebide=self.myQueue[0]
+            if treenode.left==None:
+                treenode.left=None
+                self.myQueue.append(treenode.left)
+            else:
+                treenode.right=node
+                self.myQueue.append(treenode.right)
+                self.myQueue.pop(0)
+  
+  def front_recursive(self, root):
+        if root ==None:
+            return
+        
     
       
