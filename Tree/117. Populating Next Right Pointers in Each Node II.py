4c5fff1ac905e7d616328e0f626fc880c6c45e31
ms2ag16 <misuntech16@gmail.com>
2018-08-30 14:22:27 -0400

    Create 117. Populating Next Right Pointers in Each Node II.py

---
+++ Tree/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,45 @@
+"""
+Given a binary tree
+
+struct TreeLinkNode {
+  TreeLinkNode *left;
+  TreeLinkNode *right;
+  TreeLinkNode *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+Example:
+
+Given the following binary tree,
+
+     1
+   /  \
+  2    3
+ / \    \
+4   5    7
+After calling your function, the tree should look like:
+
+     1 -> NULL
+   /  \
+  2 -> 3 -> NULL
+ / \    \
+4-> 5 -> 7 -> NULL
+
+"""
+class TreeNode(object):
+     def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next=None
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
