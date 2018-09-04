ce3017d8be251f87be485628058a15ac3d2324c2
ms2ag16 <misuntech16@gmail.com>
2018-09-04 16:03:24 -0400

    Create 144. Binary Tree Preorder Traversal.py

---
+++ Tree/Given a binary tree- return the preorder traversal of its nodes- values.  Example-  Input- -1-null-2-3-    1     -      2/144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,37 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+"""
+
+class TreeNode(object):
+    def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+
+
+if __name__=="__main__":
+    n1=TreeNode(1)
+    n2=TreeNode(2)
+    n3=TreeNode(3)
+    n4=TreeNode(4)
+    n5=TreeNode(5)
+    n6=TreeNode(6)
+    n1.left=n2
+    n1.right=n5
+    n2.left=n3
+    n2.right=n4
+    n5.right=n6
+    print Solution().sumNumbers(n1)
