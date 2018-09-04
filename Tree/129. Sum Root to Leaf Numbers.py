5e9f16793669b8d99e81aebc075e2cf21aa11e74
ms2ag16 <misuntech16@gmail.com>
2018-09-04 15:29:30 -0400

    Update 129. Sum Root to Leaf Numbers.py

--- Tree/129. Sum Root to Leaf Numbers.py
+++ Tree/129. Sum Root to Leaf Numbers.py
@@ -29,7 +29,12 @@ The root-to-leaf path 4->9->1 represents the number 491.
 The root-to-leaf path 4->0 represents the number 40.
 Therefore, sum = 495 + 491 + 40 = 1026.
 """
-
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
 class Solution(object):
     def sumNumbers(self, root):
         """
