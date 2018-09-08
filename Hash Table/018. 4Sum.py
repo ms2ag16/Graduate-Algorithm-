90dbea59485521247cd31ec13b2bc9973e501393
ms2ag16 <misuntech16@gmail.com>
2018-09-08 12:10:09 -0400

    Update 018. 4Sum.py

--- Hash Table/018. 4Sum.py
+++ Hash Table/018. 4Sum.py
@@ -23,7 +23,27 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        if len(nums)<4:
+            return []
+        res=set()
+        sumIndex={}
         
         # get sum of two nums
         for i in range(len(nums)):
+            # no duplicate same element (can be same value) in sorted sums
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] in sumIndex:
+                    sumIndex[nums[i]+nums[j]].append((i,j))
+                else:
+                    sumIndex[nums[i]+nums[j]]=((i,j))
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sumNeed=target - nums[i]-nums[j]
+                if sumNeed in sumIndex:
+                    for index in sumIndex[sumNeed]:
+                        if index[0]>j:
+                            res.add(tuple(sorted([nums[i],nums[j],nums[index[0]], nums[index[1]]])))
+        return (res=map(list,res))
+                        
+                
         
