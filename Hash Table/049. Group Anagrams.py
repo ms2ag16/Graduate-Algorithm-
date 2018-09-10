663f4a124a958e3c9f87d3c93351f29888ce6278
ms2ag16 <misuntech16@gmail.com>
2018-09-10 13:35:19 -0400

    Update 049. Group Anagrams.py

--- Hash Table/049. Group Anagrams.py
+++ Hash Table/049. Group Anagrams.py
@@ -29,6 +29,26 @@ class Solution(object):
           res[tuple(sorted(s))].append(s)
         return res.value
  
+
+class Solution2(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        map={}
+        for i, v in enumerate(strs):
+          target="".join(sorted(v))
+          if target not in map:
+            map[target]=[v]
+          else:
+            map[target].append(v)
+        res=[]
+        for value in map.values():
+          result+=[sorted(value)]
+        return result
+      
+        
 if __name__ == "__main__":
     assert Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [['nat', 'tan'], ['bat'],
                                                                                     ['ate', 'eat', 'tea']]       
