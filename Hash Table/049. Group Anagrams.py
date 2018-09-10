08d3e14a78bbeadf769baba3764bb24904127e3c
ms2ag16 <misuntech16@gmail.com>
2018-09-10 13:30:20 -0400

    Update 049. Group Anagrams.py

--- Hash Table/049. Group Anagrams.py
+++ Hash Table/049. Group Anagrams.py
@@ -16,10 +16,19 @@ All inputs will be in lowercase.
 The order of your output does not matter.
 """
 
+from collections import defaultdict
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
+        res=defaultdict(list)
+        for s in strs:
+          res[tuple(sorted(s))].append(s)
+        return res.value
+ 
+if __name__ == "__main__":
+    assert Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [['nat', 'tan'], ['bat'],
+                                                                                    ['ate', 'eat', 'tea']]       
