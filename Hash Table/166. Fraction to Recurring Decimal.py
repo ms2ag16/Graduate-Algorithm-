23d7d06aab8b5bab79835d0687a0ef967c59ccb3
ms2ag16 <misuntech16@gmail.com>
2018-09-12 16:10:07 -0400

    Update 166. Fraction to Recurring Decimal.py

--- Hash Table/166. Fraction to Recurring Decimal.py
+++ Hash Table/166. Fraction to Recurring Decimal.py
@@ -17,6 +17,7 @@ Input: numerator = 2, denominator = 3
 Output: "0.(6)"
 """
 
+
 class Solution(object):
     def fractionToDecimal(self, numerator, denominator):
         """
@@ -24,3 +25,37 @@ class Solution(object):
         :type denominator: int
         :rtype: str
         """
+        int_ans, dec_ans = "", ""
+        hmap = {}
+        idx = 0
+        # positive or negative
+        if numerator * denominator < 0:
+            int_ans += "-"
+        print "int_ans=%s"%int_ans
+        numerator, denominator = abs(numerator), abs(denominator)
+
+        mod = numerator / denominator
+        reminder = numerator % denominator
+        int_ans += str(mod)
+        if reminder != 0:
+            int_ans += "."
+
+        while reminder > 0:
+            mod = reminder * 10 / denominator
+            if reminder not in hmap:
+                dec_ans += str(mod)
+                hmap[reminder] = idx
+                idx += 1
+            else:
+                i = hmap[reminder]
+                dec_ans = dec_ans[:i] + "(" + dec_ans[i:] + ")"
+                break
+
+            reminder = reminder * 10 % denominator
+        return int_ans + dec_ans
+
+
+
+
+if __name__=="__main__":
+    print Solution().fractionToDecimal(1, 13)
