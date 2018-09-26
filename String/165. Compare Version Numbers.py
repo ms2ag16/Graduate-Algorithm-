7a34c8c6c31311fc55b63df0789c0eb55ac57494
ms2ag16 <misuntech16@gmail.com>
2018-09-26 11:38:58 -0400

    Update 165. Compare Version Numbers.py

--- String/165. Compare Version Numbers.py
+++ String/165. Compare Version Numbers.py
@@ -34,6 +34,22 @@ class Solution(object):
             elif v1<v2:
                 return -1
         return 0
+    
+    def recurse(self, version1, version2):
+        if version1==version2:
+            return 0
+
+        ind1=version1.find('.')
+        ind2 = version2.find('.')
+        part1 = version1[0:ind1] if ind1 != -1 else version1
+        part2 = version1[0:ind2] if ind2 != -1 else version2
+
+        if int(part1)==int(part2):
+            remain1=version1[len(part1)+1:] if version1[len(part1)+1:] != '' else '0'
+            remain2 = version2[len(part2) + 1:] if version2[len(part2) + 1:] != '' else '0'
+            return self.compareVersion(remain1,remain2)
+        else:
+            return 1 if int(part1)>int(part2) else -1
 
 
 
