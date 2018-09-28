fcbe6e50d292e284fb0628ca0d18b08ac641001f
ms2ag16 <misuntech16@gmail.com>
2018-09-28 12:56:13 -0400

    Update Hackland Election.py

--- TW/Hackland Election.py
+++ TW/Hackland Election.py
@@ -1,3 +1,18 @@
 """
 In elections taht use the ballot box system for voting, each voter 
 """
+def electionWinner(s):
+        vote=dict()
+        for i in range(len(s)):
+            if s[i] in vote:
+                vote[s[i]]+=1
+            else:
+                vote[s[i]]=1
+        maxIndex=sorted(vote.values(), reverse=True)[0]
+
+        sortedS=sorted(vote.keys(), reverse=True)
+        print sortedS, vote
+
+        for i in sortedS:
+            if vote[i]==maxIndex:
+                return i
