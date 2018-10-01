f99294cf208efe5f584a61ad8901bb522455fd57
ms2ag16 <misuntech16@gmail.com>
2018-10-01 15:29:14 -0400

    Create 037. Sudoku Solver.py

---
+++ Backtracking/037. Sudoku Solver.py
@@ -0,0 +1,19 @@
+"""
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+Empty cells are indicated by the character '.'.
+"""
+""" 数独"""
+
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        
