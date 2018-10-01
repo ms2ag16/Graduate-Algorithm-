359b9949cdfa778b69a9ec43d93f8a114b6ebb2f
ms2ag16 <misuntech16@gmail.com>
2018-10-01 15:12:57 -0400

    Update 022. Generate Parentheses.py

--- Backtracking/022. Generate Parentheses.py
+++ Backtracking/022. Generate Parentheses.py
@@ -18,4 +18,15 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        def generate(left, right, string, res):
+            if left==n and right==n:
+                return res.append(string)  
+            
+            if left<n:
+                generate(left+1, right, string+"(", res)
+            if left>right:
+                generate(left, right+1, string+")", res)
+        res=[]
+        generate(0,0,"", res)
+        return res
         
