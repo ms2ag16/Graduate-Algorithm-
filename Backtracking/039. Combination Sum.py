37087bd40b3332f56c81bc5c0738d4c5bc45b06c
ms2ag16 <misuntech16@gmail.com>
2018-10-01 15:47:10 -0400

    Update 039. Combination Sum.py

--- Backtracking/039. Combination Sum.py
+++ Backtracking/039. Combination Sum.py
@@ -1,10 +1,9 @@
 """
-Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
-
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sums to target.
 The same repeated number may be chosen from candidates unlimited number of times.
 
 Note:
-
 All numbers (including target) will be positive integers.
 The solution set must not contain duplicate combinations.
 Example 1:
