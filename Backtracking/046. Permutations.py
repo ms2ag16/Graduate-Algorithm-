45d5f599aa3bc59b64f7e39a33b11ba15dace918
ms2ag16 <misuntech16@gmail.com>
2018-10-02 14:51:13 -0400

    Create 046. Permutations.py

---
+++ Backtracking/046. Permutations.py
@@ -0,0 +1,24 @@
+"""
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
