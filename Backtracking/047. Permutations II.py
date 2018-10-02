8bd60d40d6ead295f45408ad25209801ab76270d
ms2ag16 <misuntech16@gmail.com>
2018-10-02 16:40:23 -0400

    Create 047. Permutations II.py

---
+++ Backtracking/047. Permutations II.py
@@ -0,0 +1,21 @@
+"""
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
