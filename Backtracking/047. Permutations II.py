8573c436e23b88c2eb343d5345db921b2d12f5fe
ms2ag16 <misuntech16@gmail.com>
2018-10-02 16:52:15 -0400

    Update 047. Permutations II.py

--- Backtracking/047. Permutations II.py
+++ Backtracking/047. Permutations II.py
@@ -18,4 +18,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        res=[]
+        nums.sort()
+        self.dfs(nums, res,[])
+        return res
+
+    def dfs(self, nums, res, current):
+        if not nums:
+            res.append(current)
+        else:
+            for i in range(len(nums)):
+                if nums[i]==nums[i-1] and i-1>=0:
+                    continue
+                self.dfs(nums[i+1:]+nums[:i], res, current + [nums[i]])
