28864374261ee4476a551b67f0cdbbbb93684376
ms2ag16 <misuntech16@gmail.com>
2018-10-10 15:13:42 -0400

    Create 215. Kth Largest Element in an Array.py

---
+++ Divide_and_Conquer/215. Kth Largest Element in an Array.py
@@ -0,0 +1,36 @@
+"""
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+"""
+
+""" DIVIDE AND CONQUER """
+class Solution(object):
+       def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        
+        
+        
+class Solution(object):
+       def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        length=len(nums)
+        return nums[length-k]
+        
