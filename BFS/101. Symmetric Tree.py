d109ee9eabcead4379033dabf1178e2ad6ce20fa
ms2ag16 <misuntech16@gmail.com>
2018-10-24 19:41:00 -0400

    Update 101. Symmetric Tree.py

--- BFS/101. Symmetric Tree.py
+++ BFS/101. Symmetric Tree.py
@@ -6,10 +6,26 @@ class TreeNode(object):
          self.left = None
          self.right = None
          
-class Solution(object):
-    
+class Solution(object): 
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        from collections import deque
+        lQueue, rQueue= deque([root.left]), deque([root.right])
+        while lQueue and rQueue:
+            left=lQueue.pop()
+            right=rQueue.pop()
+            if not left and not right:
+                continue
+            elif not left or not right:
+                return False
+            elif left.val!=right.val:
+                return False
+            lQueue.append(left.left)
+            lQueue.append(left.right)
+            rQueue.append(right.right)
+            rQueue.append(right.left)
+        return len(lQueue)==len(rQueue)
+    
