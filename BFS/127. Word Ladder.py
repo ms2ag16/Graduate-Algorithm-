c841fb263c8686057b733d477a3bc6d810d3483f
ms2ag16 <misuntech16@gmail.com>
2018-10-25 14:31:36 -0400

    Update 127. Word Ladder.py

--- BFS/127. Word Ladder.py
+++ BFS/127. Word Ladder.py
@@ -1,3 +1,13 @@
+""" 
+use BFS to go over the all the child node which are only 1 letter different than the current checking word.
+To check if the word is only 1 letter different than the next one, 
+The idea is to loopong over ascii_lowercase 26 letters, and if it's only 1 different and then we check if it is in the wordList,
+if it does in the wordList, we add the nextWord into the queue, increase the count step number and from there to seach for the endWord;
+if it not in the wordList, we keep checking other letters in ascii_lowercase.
+Since there's no duplicate and we use one word once, so after we add the newWord to queue, we remove it from wordList
+"""
+
+
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
         """
