0196c0fc77c2f5c2651a4654a00a6255cd8303ed
ms2ag16 <misuntech16@gmail.com>
2018-10-31 16:42:31 -0400

    Update 110. Balanced Binary Tree.py

--- Tree/110. Balanced Binary Tree.py
+++ Tree/110. Balanced Binary Tree.py
@@ -16,8 +16,12 @@ Given the following tree [3,9,20,null,null,15,7]:
    15   7
 Return true.
 
-Solution1, calculate every depth for every node, it's a waste.
+Solution1, calculate every depth for every node, it's a waste. Recursion Idea, 2 Recursion, 1st calculate maxDepth of a tree
+second, 判断每个节点的两个子树高度差是否小于等于1
 
+Solution2, using dfs idea to calculate 在求出每个节点高度的同时，判断是否是平衡二叉树。 
+用DFS来计算高度，若高度差超过1，则返回-1.否则，返回高度。
+理论上说，对每个节点的访问应该返回两个值：是否平衡、节点高度，为了节省空间，若用-1表示不平衡的状态则可省去一个变量
 """
 
 class TreeNode(object):
@@ -46,17 +50,21 @@ class Solution(object):
         return self.isBalanced(root.left) and self.isBalanced(root.right)
     
 class Solution2(object):
-    def maxDepth(self, root):
-        if root==None:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
-    
-
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        return self.dfs(root)!=-1
+    
+    def dfs(self,root):
+        if not root:
+            return 0
+        left, right=self.dfs(root.left), self.dfs(root.right)
+        if left<0 or right<0 or abs(left-right)>1:
+            return -1
+        return max(left, right)+1
+        
 
 if __name__ == "__main__":
     n1 = TreeNode(0)
