1a3d88c1f97b591f786f0a8f63682f26c894e56c
ms2ag16 <misuntech16@gmail.com>
2018-11-05 11:19:58 -0500

    Update 111. Minimum Depth of Binary Tree.py

--- DFS/111. Minimum Depth of Binary Tree.py
+++ DFS/111. Minimum Depth of Binary Tree.py
@@ -4,7 +4,37 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+""" iteration version """
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        stack=[root]
+        curr=1
+        if not root:
+            return 0
+        while stack:
+            for i in range(len(stack)):
+                top=stack.pop(0)
+                if top.left:
+                    stack.append(top.left)
+                if top.right:
+                    stack.append(top.right)
+                if not top.right and not top.left:
+                    return curr
+            curr+=1
+        return curr
+    
+""" recursion version """
 class Solution(object):
     def minDepth(self, root):
         """
@@ -22,3 +52,5 @@ class Solution(object):
         if root.right is None:
             return self.dfs(root.left)+1
         return min(self.dfs(root.left), self.dfs(root.right))+1
+
+    
