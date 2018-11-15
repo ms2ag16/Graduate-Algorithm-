be41261fb3fd6e894f6f3443a36cdfdeddb4b586
Mi Sun <msun85@gatech.edu>
2018-11-14 22:01:49 -0500

    update

--- Heap/347. Top K Frequent Elements.py
+++ Heap/347. Top K Frequent Elements.py
@@ -16,6 +16,29 @@ class Solution(object):
             res.append(sorted_count[i][0])
         print (res)
 
+""" using heap"""
+class Solution1(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        import heapq
+        from collections import defaultdict
+        count=defaultdict(int)
+        heap, res = [], []
+        for num in nums:
+            count[num]+=1
+        for key, value in count.items():
+            entry=[value,key]
+            heapq.heappush(heap,entry)
+        for i in heapq.nlargest(k,heap):
+            res.append(i[1])
+        return sorted(res)
+
+
+
 
 if __name__=="__main__":
-    print (Solution().topKFrequent([1,1,1,2,2,2,3,4,5,6,6],2))
+    print (Solution1().topKFrequent([1,1,1,2,2,2,3,4,5,6,6],2))
