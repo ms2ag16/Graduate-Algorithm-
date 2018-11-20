f20f6b7f12446df7681bc185ee2c9705f5bafc0d
ms2ag16 <misuntech16@gmail.com>
2018-11-20 15:03:50 -0500

    Create 122. Best Time to Buy and Sell Stock II.py

---
+++ Dynamic_Programming/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,31 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+"""
+
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        max_profit=0
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                max_profit+=prices[i]-prices[i+1]
+        return max_profit
+            
+            
+        
