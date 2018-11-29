1ac76a65427f1edb0276bb72147729ef6705d521
ms2ag16 <misuntech16@gmail.com>
2018-11-29 11:34:36 -0500

    Create Match String.py

---
+++ Google/Match String.py
@@ -0,0 +1,3 @@
+Given two strings, such as str="BABA", str2="CAB".  The goal is to from str1 by concatenating multiple copies of str2, before concatenating a copy of str2, you can remove any number of chars from it.
+1. Is it possbile to form str1, given str2?
+2. What is the minimum number of copies?
