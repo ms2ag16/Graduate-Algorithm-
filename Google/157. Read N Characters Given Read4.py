fa4ae5d467224b349ef22e88af1655bbf87ad9cf
ms2ag16 <misuntech16@gmail.com>
2018-11-29 14:32:27 -0500

    Update 157. Read N Characters Given Read4.py

--- Google/157. Read N Characters Given Read4.py
+++ Google/157. Read N Characters Given Read4.py
@@ -27,4 +27,15 @@ class Solution(object):
         :type n: Maximum number of characters to read (int)
         :rtype: The number of characters read (int)
         """
+        idx=0
+        buf4=[""]*4             """         ["","","",""]         """
+        l4=read4(buf4)
+        while n>0:
+            for i in range(min(l4,n)):
+                buf[idx]=buf4[i]
+                idx+=1
+                n-=1
+        return idx
+               
+               
         
