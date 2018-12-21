e9aa6dedaa7b639f12a6f701c93f80591f8e2513
ms2ag16 <misuntech16@gmail.com>
2018-12-21 16:46:06 -0500

    Update BinaryTree.py

--- Google/BinaryTree.py
+++ Google/BinaryTree.py
@@ -1,5 +1,5 @@
 class TreeNode(object):
-  def __init__(self, x):
+  def __init__(self, x=-1):
     self.val=x
     self.left=None
     self.right=None
@@ -10,7 +10,20 @@ class Tree(object):
   def __init__(self):
       self.root=TreeNode()
       self.queue=[]
-  def add(self, ele):
+  def add(self, x):
+      node=TreeNode(x)
+      if self.root is None:
+          self.root=node
+          self.queue.append(self.root)
+      else:
+          treeNode=self.queue[0]
+          if treeNode.left==None:
+              treeNode.left=node
+              self.queue.append(treeNode.left)
+          else:
+              treeNode.right=node
+              self.queue.append(treeNode.right)
+              self.queue.pop(0)
     
   
     
