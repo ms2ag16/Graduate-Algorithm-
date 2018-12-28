51d30fa911ecae0c7119e5d33c3266bec8171ec1
ms2ag16 <misuntech16@gmail.com>
2018-12-28 17:00:26 -0500

    Update Binary Tree.py

--- Tree/Binary Tree.py
+++ Tree/Binary Tree.py
@@ -1,3 +1,81 @@
+"""
+Non-Recursion- preorder
+"""
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack=[root]
+        preorder=[]
+        if not root:
+            return preorder
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        inorder=[]
+        stack=[]
+        curt = root
+        while curt or stack:
+            while curt:
+                stack.append(curt)
+                curt=curt.left
+            if stack:
+                curt=stack.pop()
+                inorder.append(curt.val)
+                curt=curt.right
+        return inorder
+
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        postorder=[]
+        stack=[root]
+        prev = TreeNode(None)
+        curr = root
+
+        if not root:
+            return None
+        while stack:
+            curr = stack.pop()
+            if not prev or not prev.left or not prev.right: # traverse down the tree
+                if curr.left:
+                    stack.append(curr.left)
+                elif curr.right:
+                    stack.append(curr.right)
+            elif curr.left == prev: # traverse up the tree from the left
+                if curr.right:
+                    stack.append(curr.right)
+            else:   # traverse up the tree from the right
+                postorder.append(curr.val)
+            prev = curr
+        return postorder
+
+
 from Tree import Tree
 
 
@@ -33,3 +111,4 @@ class BinaryTree(Tree):
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
+    
