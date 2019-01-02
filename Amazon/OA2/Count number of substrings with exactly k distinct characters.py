7e8440b60dc0e2b824a03a899326ca06de1761c0
ms2ag16 <misuntech16@gmail.com>
2019-01-02 15:47:38 -0500

    Create  Count number of substrings with exactly k distinct characters.py

---
+++ Amazon/OA2/Count number of substrings with exactly k distinct characters.py
@@ -0,0 +1,16 @@
+"""
+Given a string of lowercase alphabets, count all possible substrings (not necessarily distinct) that has exactly k distinct characters.
+Examples:
+
+Input: abc, k = 2
+Output: 2
+Possible substrings are {"ab", "bc"}
+
+Input: aba, k = 2
+Output: 3
+Possible substrings are {"ab", "ba", "aba"}
+
+Input: aa, k = 1
+Output: 3
+Possible substrings are {"a", "a", "aa"}
+"""
