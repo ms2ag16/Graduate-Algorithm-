433ffb932a8174a446cbe0facf4c624811ac0d88
ms2ag16 <misuntech16@gmail.com>
2019-01-04 13:51:19 -0500

    Create Longest Substring with at Most k Distinct Characters.py

---
+++ Amazon/OA2/Longest Substring with at Most k Distinct Characters.py
@@ -0,0 +1,8 @@
+""" 
+给定一个字符串，找到最多有k个不同字符的最长子字符串。
+Given a string s, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = "eceba", k = 3,
+T is "eceb" which its length is 4.
+"""
+
